b7dec264ad83617ff554f547cc22d72aab87c6d3
fumiya369 <iijima0609fumiya@gmail.com>
2024-06-05 11:53:15 +0900

    2回目コミット

--- Dentaku/src/dentaku/Dentaku.java
+++ Dentaku/src/dentaku/Dentaku.java
@@ -1,11 +1,10 @@
 package dentaku;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Dentaku {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		
 		int i = 0; //変数を初期化
 		double sum = 0; //合計値
@@ -14,42 +13,42 @@ public class Dentaku {
 		for(; i < 1;) { //繰り返す
 			System.out.println("演算子を入力してください。");
 			System.out.println("足し算：+、引き算：-、掛け算：*、割り算：/");
-			String operator = br.readLine(); //演算子
+			String operator = sc.nextLine(); //演算子
 			
 			if(operator.equals("+")) { //足し算
 				System.out.println("足される数を入力してください。");
-				double value1 = Double.parseDouble(br.readLine()); //小数
+				double value1 = Double.parseDouble(sc.next()); //小数
 				System.out.println("足す数を入力してください。");
-				double value2 = Double.parseDouble(br.readLine());
+				double value2 = Double.parseDouble(sc.next());
 				sum = value1 + value2;
 				System.out.println(value1 + " + " + value2 + " = " + sum + " です。");
 			}
 			else if(operator.equals("-")) { //引き算
 				System.out.println("引かれる数を入力してください。");
-				double value1 = Double.parseDouble(br.readLine()); //小数
+				double value1 = Double.parseDouble(sc.next()); //小数
 				System.out.println("引く数を入力してください。");
-				double value2 = Double.parseDouble(br.readLine());
+				double value2 = Double.parseDouble(sc.next());
 				sum = value1 - value2;
 				System.out.println(value1 + " - " + value2 + " = " + sum + " です。");
 			}
 			else if(operator.equals("*")) { //掛け算
 				System.out.println("掛けられる数を入力してください。");
-				double value1 = Double.parseDouble(br.readLine()); //小数
+				double value1 = Double.parseDouble(sc.next()); //小数
 				System.out.println("掛ける数を入力してください。");
-				double value2 = Double.parseDouble(br.readLine());
+				double value2 = Double.parseDouble(sc.next());
 				sum = value1 * value2;
 				System.out.println(value1 + " * " + value2 + " = " + sum + " です。");
 			}
 			else if(operator.equals("/")) { //割り算
 				System.out.println("割られる数を入力してください。");
-				double value1 = Double.parseDouble(br.readLine()); //小数
+				double value1 = Double.parseDouble(sc.next()); //小数
 				System.out.println("割る数を入力してください。");
-				double value2 = Double.parseDouble(br.readLine());
+				double value2 = Double.parseDouble(sc.next());
 				if(value2 == 0) { //0を入力された場合
 					System.out.println("0では割れません。");
 					System.out.println("もう一度計算しますか。");
 					System.out.println("はい：Y いいえ：N");
-					again = br.readLine();
+					again = sc.nextLine();
 					if(again.equals("Y")) {
 						continue;
 					}
@@ -70,7 +69,7 @@ public class Dentaku {
 			for(; i < 1;) {
 				System.out.println("もう一度計算しますか。");
 				System.out.println("はい：Y いいえ：N");
-				again = br.readLine();
+				again = sc.nextLine();
 				if(again.equals("Y")) {
 					break;
 				}
